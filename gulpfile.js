86bf85c88636696b634d7a9722e5c22f1d3f5702
Tatiana Kibireva <t-kibireva16@yandex.ru>
2025-03-06 12:12:16 +0300

    исправлены замечания по второй проверке

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,50 @@ const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+  });
+}
+
 function html() {
-  return gulp.src('src/**/*.html')
-         .pipe(plumber())
-                 .pipe(gulp.dest('dist/'))
+  return gulp
+    .src('src/**/*.html')
+    .pipe(plumber())
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function css() {
-  return gulp.src('src/styles/**/*.css')
-        .pipe(plumber())
-        .pipe(concat('bundle.css'))
-                .pipe(gulp.dest('dist/'))
+  return gulp
+    .src('src/styles/**/*.css')
+    .pipe(plumber())
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function images() {
-  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-            .pipe(gulp.dest('dist/images'))
+  return gulp
+    .src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function fonts() {
-  return gulp.src('src/fonts/**/*.{css,woff2,woff}')
-            .pipe(gulp.dest('dist/fonts'))
+  return gulp
+    .src('src/fonts/**/*.{css,woff2,woff}')
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function scripts() {
-  return gulp.src('src/scripts/**/*.js')
-            .pipe(gulp.dest('dist/scripts'))
+  return gulp
+    .src('src/scripts/**/*.js')
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function clean() {
@@ -37,11 +55,23 @@ function clean() {
 }
 
 function svg() {
-  return gulp.src('src/svg/**/*.svg')
-            .pipe(gulp.dest('dist/svg'))
+  return gulp
+    .src('src/svg/**/*.svg')
+    .pipe(gulp.dest('dist/svg'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images, fonts, scripts, svg));
+function watchFiles() {
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/blocks/**/*.css'], css);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+}
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(html, css, images, fonts, scripts, svg)
+);
+const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.svg = svg;
 exports.scripts = scripts;
@@ -50,4 +80,6 @@ exports.images = images;
 exports.css = css;
 exports.html = html;
 exports.clean = clean;
-exports.build = build; 
+exports.build = build;
+exports.watchapp = watchapp;
+exports.default = watchapp;
